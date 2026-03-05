642dff9759607adc7b96ecd46c8c58df3b92ba88
TheDigitalDaVinci <evoluzionemillenio@gmail.com>
2026-03-05 13:01:46 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,305 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entry",
+      "name": "Entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.20799945566361977,
+        "pitch": 0.030132823800187225,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.655247908224073,
+          "pitch": 0.12235973553705826,
+          "rotation": 0,
+          "target": "1-center"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-center",
+      "name": "Center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.01793157142288848,
+        "pitch": -0.002511068650015602,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9225497448140256,
+          "pitch": 0.05829751271253514,
+          "rotation": 0,
+          "target": "2-premium"
+        },
+        {
+          "yaw": -0.01011815785228265,
+          "pitch": -0.0500708115289612,
+          "rotation": 0,
+          "target": "3-earring-section"
+        },
+        {
+          "yaw": -0.7737346024141889,
+          "pitch": 0.09384139143347525,
+          "rotation": 0,
+          "target": "4-cosmetics-section"
+        },
+        {
+          "yaw": 3.0305130727127185,
+          "pitch": 0.03697171219823936,
+          "rotation": 0,
+          "target": "0-entry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-premium",
+      "name": "Premium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.09683132733685973,
+        "pitch": -0.002511068650015602,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8336558168772257,
+          "pitch": 0.008243057905541917,
+          "rotation": 0,
+          "target": "0-entry"
+        },
+        {
+          "yaw": -2.9235358820208592,
+          "pitch": 0.01667523135569482,
+          "rotation": 0,
+          "target": "4-cosmetics-section"
+        },
+        {
+          "yaw": -1.9952458412108331,
+          "pitch": 0.11591875874433732,
+          "rotation": 0,
+          "target": "3-earring-section"
+        },
+        {
+          "yaw": 2.64119074661323,
+          "pitch": 0.31324199240916073,
+          "rotation": 0,
+          "target": "1-center"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-earring-section",
+      "name": "Earring Section",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.139737591555681,
+        "pitch": 0.24931351780118582,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4077741440967628,
+          "pitch": 0.19202929335661878,
+          "rotation": 0,
+          "target": "2-premium"
+        },
+        {
+          "yaw": -0.14846659976922005,
+          "pitch": 0.10598043764677456,
+          "rotation": 0,
+          "target": "0-entry"
+        },
+        {
+          "yaw": 1.3617649131232596,
+          "pitch": 0.2503221081277722,
+          "rotation": 0,
+          "target": "4-cosmetics-section"
+        },
+        {
+          "yaw": 0.021649273905827826,
+          "pitch": 0.4396686934207015,
+          "rotation": 0,
+          "target": "1-center"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cosmetics-section",
+      "name": "Cosmetics Section",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07710529914631614,
+        "pitch": 0.004950307895166262,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.078093047481632,
+          "pitch": 0.09403865233533182,
+          "rotation": 0,
+          "target": "3-earring-section"
+        },
+        {
+          "yaw": -0.29109980117794976,
+          "pitch": 0.10917012960844552,
+          "rotation": 0,
+          "target": "1-center"
+        },
+        {
+          "yaw": 2.866509555632282,
+          "pitch": 0.1325524856574205,
+          "rotation": 0,
+          "target": "5-pantry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pantry",
+      "name": "Pantry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3484113987910966,
+        "pitch": 0.03515178913125183,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.890492207048844,
+          "pitch": 0.09997013767205587,
+          "rotation": 0,
+          "target": "4-cosmetics-section"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Kaitharan Jewels",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
